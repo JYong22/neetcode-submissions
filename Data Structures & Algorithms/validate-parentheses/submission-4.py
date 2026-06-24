8a6ca1321a69beeea148968513eab006e910a46b
J-Yong-22 <59133484+JYong22@users.noreply.github.com>
2026-06-24 13:55:38 -0400

    Add: validate-parentheses - submission-4

---
+++ Data Structures & Algorithms/validate-parentheses/submission-4.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+
+        stack = []
+        thisDict = {']': '[', '}': '{', ')': '('}
+
+        for c in s:
+            if stack and c in thisDict and stack[-1] == thisDict[c]:
+                stack.pop()
+            else:
+                stack.append(c)
+
+        return len(stack) == 0
+        
